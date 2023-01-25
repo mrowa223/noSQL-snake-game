bc6db3ceabe888e9cf38a364280805ed28c98a4b
eegurt <alihan28.06.0450@gmail.com>
2023-01-25 21:39:48 +0600

    db + user model

--- server/main.js
+++ server/main.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+require('dotenv').config()
 
 const app = express()
 const PORT = config.get('port')
 
 const start = async () => {
 	try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.DB_URI)
         
         app.listen(PORT, () => {
             console.log(`Server started listening on ${PORT} port`)
---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose');
+
+const User = new Schema({
+    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+})
+// TODO: other models
+
+module.exports = model("User", User)
