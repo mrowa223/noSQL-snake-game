b1681c0c6f98e289ca6f74762035d211781ca1a7
eegurt <alihan28.06.0450@gmail.com>
2023-01-29 22:26:16 +0600

    user registration

--- server/main.js
+++ server/main.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const authRouter = require('./routes/auth')
 require('dotenv').config()
+
+mongoose.set('strictQuery', true)
 
 const app = express()
 const PORT = config.get('port')
 
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
 const start = async () => {
 	try {
-        await mongoose.connect(process.env.DB_URI)
-        
-        app.listen(PORT, () => {
-            console.log(`Server started listening on ${PORT} port`)
-        })
+		await mongoose.connect(process.env.DB_URI)
+
+		app.listen(PORT, () => {
+			console.log(`Server started listening on ${PORT} port`)
+		})
 	} catch (e) {
 		console.log(e)
 	}
---
+++ server/routes/auth.js
@@ -0,0 +1,74 @@
+const { Router } = require('express')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const { check, validationResult } = require('express-validator')
+
+const router = new Router()
+
+const validation = [
+	check('email').isEmail().withMessage('Incorrect e-mail').normalizeEmail(),
+	check('username')
+		.isLength({ min: 4, max: 20 })
+		.withMessage(
+			'Username must be longer than 4 characters and shorter than 20'
+		)
+		.matches(/^[a-zA-Z]/)
+		.withMessage('Username must start with letter')
+		.matches(/^\w+$/)
+		.withMessage(
+			'Username must consist only of letters, digits and _ (underscore character)'
+		),
+	check('password')
+		.isLength({ min: 4, max: 20 })
+		.withMessage(
+			'Password must be longer than 4 characters and shorter than 20'
+		)
+]
+
+router.post('/signup', validation, async (req, res) => {
+	try {
+		console.log(req.body)
+
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ message: 'Validation error', errors })
+		}
+
+		const { email, username, password } = req.body
+
+		let candidate = await User.findOne({ email })
+		if (candidate) {
+			return res
+				.status(400)
+				.json({ message: `User with '${email}' e-mail already exists` })
+		}
+		candidate = await User.findOne({
+			username: { $regex: new RegExp(username, 'i') }
+		})
+		if (candidate) {
+			return res
+				.status(400)
+				.json({
+					message: `User with '${username}' username already exists`
+				})
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 7)
+
+		const user = new User({ email, username, password: hashedPassword })
+		await user.save()
+
+		return (
+			res
+				// .status(201)
+				.json({
+					message: `User ${username} has been succesfully created`
+				})
+		)
+	} catch (e) {
+		console.log(e)
+		res.send({ message: 'Server error' })
+	}
+})
+
+module.exports = router
